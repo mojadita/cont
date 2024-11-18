2ef7799d110cda25b2a3c41b840b09115e64cd25
Luis Colorado <luiscoloradourcola@gmail.com>
2024-11-18 17:03:46 +0200

    Add a -i flag to ignore subprocess errors and continue executing
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- cont.c
+++ cont.c
@@ -25,20 +25,21 @@
 #error set VERSION in makefile
 #endif
 
-#define FLAG_VERBOSE    (1<<0)
-#define FLAG_VERSION    (1<<1)
-#define FLAG_DELAY      (1<<2)
-#define FLAG_NTIMES     (1<<3)
-#define FLAG_STOP       (1<<4)
-#define FLAG_NOESCAPES  (1<<5)
-#define FLAG_REDSTDERR  (1<<6)
-#define FLAG_PROGRESS   (1<<7)
+#define FLAG_VERBOSE     (1<<0)
+#define FLAG_VERSION     (1<<1)
+#define FLAG_DELAY       (1<<2)
+#define FLAG_NTIMES      (1<<3)
+#define FLAG_STOP        (1<<4)
+#define FLAG_NOESCAPES   (1<<5)
+#define FLAG_REDSTDERR   (1<<6)
+#define FLAG_PROGRESS    (1<<7)
+#define FLAG_IGNOREERROR (1<<8)
 
 int flags = 0;
 useconds_t delay = 1000000;
 size_t ntimes;
 
-static char *cont_cur_up = "[[[CONT_CUR_UP]]]";
+static char *cont_cur_up  = "[[[CONT_CUR_UP]]]";
 static char *cont_clr_eol = "[[[CONT_CLR_EOL]]]";
 static char *cont_clr_eos = "[[[CONT_CLR_EOS]]]";
 
@@ -55,11 +56,12 @@ void doVersion(void)
         "cont: " VERSION "\n"
         "  (C) 2019 Luis Colorado.  All rights reserved.\n"
         "  License: BSD\n"
-        "Usage: cont [ -Vev ] [ -n num ] [ -t timespec ] command [ args ... ]\n"
+        "Usage: cont [ -eipVv ] [ -n num ] [ -t timespec ] command [ args ... ]\n"
         "Where:\n"
         "  -e disables the output of escape sequences.  The program limits to\n"
         "     execute and pass the whole output to stdout.  No interspersion\n"
         "     of escapes is done.\n"
+		"  -i ignore errors from subprocess and continue executing it.\n"
         "  -n makes only `num' executions and ends after all.  If not\n"
         "     specified, the program runs until it is interrupted by a signal.\n"
         "  -p shows a progress symbol rotating at bottom of output.\n"
@@ -81,57 +83,92 @@ ssize_t loop(int argc_unused, char **argv)
 {
     int fd[2];
     int res = pipe(fd);
+	if (res < 0) {              /* PIPE ERROR */
+		fprintf(stderr,
+			F("pipe: ERROR %d: %s\n"),
+			errno,
+			strerror(errno));
+		return -1;
+	}
 
     res = fork();
     if (res < 0) {              /* FORK ERROR */
+
         fprintf(stderr,
             F("fork: ERROR %d: %s\n"),
             errno,
             strerror(errno));
+
+		close(fd[0]);
+		close(fd[1]);           /* throw the pipe */
+
         return -1;
+
     } else if (res == 0) {      /* CHILD PROCESS */
+
         close(fd[0]);           /* not going to use it */
+
         dup2( fd[1], 1);        /* redirect output to pipe */
-        if (flags & FLAG_REDSTDERR)
+        if (flags & FLAG_REDSTDERR) {
             dup2(fd[1], 2);     /* redirect stderr of child */
-        close(fd[1]);
+		}
+        close(fd[1]);           /* once dup(), we don't need it */
 
         execvp(argv[0], argv);
 
         fprintf(stderr,
             F("execv: %s: ERROR %d: %s\n"),
             argv[0], errno, strerror(errno));
+
         exit(EXIT_FAILURE);     /* DON'T RETURN */
+
     } else {                    /* PARENT PROCESS */
+
         pid_t cld_pid = res;
-        close(fd[1]); /* no writing to the pipe */
+
+        close(fd[1]);           /* no writing to the pipe */
+
         FILE *f = fdopen(fd[0], "rt"); /* just reading */
         int c;
-        size_t n_lines = 0; /* need to count lines */
+        size_t n_lines = 0;     /* need to count lines */
+
+		/* copy input to output until EOF, which can happen:
+		 *  o when the subprocess has ended.
+		 *  o when subprocess has closed it's side of the pipe.
+		 */
         while((c = fgetc(f)) != EOF) {
-            putchar(c);
-            if (c == '\n') {
+            putchar(c);         /* copy to stdout verbatim */
+            if (c == '\n') {    /* we count the newlines */
                 n_lines++;
                 if (!(flags & FLAG_NOESCAPES)) {
-                    /* DELETE TO END OF LINE */
+                    /* DELETE TO END OF LINE.  This eliminates
+					 * the extra chars in case the previous run
+					 * wrote more characters than we do now. */
                     tputs(cont_clr_eol, 1, putchar);
                 }
             }
         }
+
         if (!(flags & FLAG_NOESCAPES)) {
-            /* DELETE TO END OF SCREEN */
-            tputs(cont_clr_eos, 1, putchar);
+            /* DELETE TO END OF SCREEN.
+			 * As we have ended output, in case there's some
+			 * output from previous runs, we erase until end of screen
+			 * to clean the rest of the screen. */
+            tputs(cont_clr_eos, 1, putchar);   /* clear to end of screen */
             fflush(stdout);
         }
+
         int status;
         res = wait(&status);
         fclose(f); /* should close fd[0] */
+
         if (   res >= 0
             && WIFEXITED(status)
             && WEXITSTATUS(status) == EXIT_SUCCESS) {
+			/* child ended successfully */
             assert(res == cld_pid);
-            return n_lines;                       /* CORRECT RETURN */
         } else {
+
             if (res < 0) {                      /* wait error */
                 fprintf(stderr,
                     F("WAIT: %s\n"),
@@ -146,8 +183,11 @@ ssize_t loop(int argc_unused, char **argv)
                     F("child process %d exited with code = %d\n"),
                     cld_pid, WEXITSTATUS(status));
             }
-            return -1;                          /* ERROR RETURN */
+			if (!(flags & FLAG_IGNOREERROR)) {
+            	return -1;                      /* ERROR RETURN */
+			}
         } /* if */
+        return n_lines;                         /* CORRECT RETURN */
     } /* PARENT PROCESS */
 } /* loop */
 
@@ -158,28 +198,23 @@ int main(int argc, char **argv)
 
     while ((opt = getopt(argc, argv, "ern:pt:Vv")) >= 0) {
         switch(opt) {
-        case 'e': flags |= FLAG_NOESCAPES;
-                  break;
+        case 'e': flags |= FLAG_NOESCAPES;   break;
+		case 'i': flags |= FLAG_IGNOREERROR; break;
         case 'n': flags |= FLAG_NTIMES;
-                  ntimes = atoi(optarg);
-                  break;
-        case 'p': flags |= FLAG_PROGRESS;
-                  break;
-        case 'r': flags |= FLAG_REDSTDERR;
-                  break;
+                  ntimes = atoi(optarg);     break;
+        case 'p': flags |= FLAG_PROGRESS;    break;
+        case 'r': flags |= FLAG_REDSTDERR;   break;
         case 't': flags |= FLAG_DELAY;
-                  t = atof(optarg);
-                  break;
-        case 'V': flags |= FLAG_VERSION;
-                  break;
-        case 'v': flags |= FLAG_VERBOSE;
-                  break;
+                  t = atof(optarg);          break;
+        case 'V': flags |= FLAG_VERSION;     break;
+        case 'v': flags |= FLAG_VERBOSE;     break;
         /* ... */
         }
     }
 
-    if (flags & FLAG_VERSION)
+    if (flags & FLAG_VERSION) {
         doVersion();
+	}
 
     /* the next pair of sentences is like `shift optind' in the shell. */
     /* trick, don't move the parameters, just move the pointer */
@@ -224,6 +259,7 @@ int main(int argc, char **argv)
     size_t total_lines = 0;
     size_t total_execs = 0;
     ssize_t n = 0;
+
     while(cont && (!(flags & FLAG_NTIMES) || ntimes--)) {
 
         /* move up as many lines as input from subcommand */
@@ -254,6 +290,7 @@ int main(int argc, char **argv)
         total_lines += n;
         total_execs++;
     }
+
     if (flags & FLAG_VERBOSE) {
         tputs("\r", 1, putchar);
         fprintf(stderr,
